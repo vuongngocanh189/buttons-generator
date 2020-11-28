27ff18d41487a85758580f1ba3d5a57d8bb86ecc
vuongngocanh189 <vuongngocanh189@yahoo.com>
2020-11-28 22:43:32 +0200

    Initiate input data in an object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-/**
- * Main functions
- */
+// const TEXT = document.querySelector('text').value;
+// const FONT = document.querySelector('font-family').value;
+const data = {
+  'text': document.getElementById('text').value,
+  'font': document.getElementById('font-family').value,
+  'font-size': document.getElementById('range').value,
+  'font-color': document.getElementById('font-color').value,
+  'border-color': document.getElementById('border-color').value,
+  'bg-color': document.getElementById('bg-color').value,
+  'hover-color': document.getElementById('hover-color').value,
+}
+
+window.data = data; // For testing in the browser
